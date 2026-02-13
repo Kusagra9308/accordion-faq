ff6e3953d98891641de1d44f6765f000f8bc73ca
Kusagra Singh Chauhan <kushagrasinghchauhan6@gmail.com>
2026-02-13 17:09:29 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -10,25 +10,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/test", (req, res) => {
-  res.send("Server is alive");
-});
-app.get("/api/questions", (req, res) => {
-  res.send("SERVER LEVEL ROUTE HIT");
-});
 
 app.use("/api/questions", questionRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
